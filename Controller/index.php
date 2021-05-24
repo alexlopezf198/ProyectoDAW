aab63f3189e7907b3ffc7bdfee865b934731aa15
Alejandro Lopez <alejandro.lopez-arjona@iesruizgijon.com>
2021-05-24 15:18:47 +0200

    Version pre-alpha

---
+++ Controller/index.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+include '../View/acceso.php';
+
+
--- View/acceso.php
+++ View/acceso.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="../View/css/estilos.css">
     <title>Sistema de gestión de incidencias</title>
   </head>
   <body class="h-100">
